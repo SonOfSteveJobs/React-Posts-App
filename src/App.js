a2cbb4281696b6699840fd09cab316872bdeafba
SonOfSteveJobs <evgenev2222@gmail.com>
2023-03-19 15:41:08 +0300

    added posts render

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './styles/App.css';
 import PostItem from './components/PostItem';
 
 function App() {
-    const [value, setValue] = useState('Some text')
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Programming language.'},
+        {id: 2, title: 'Python', body: 'Another programming language.'},
+        {id: 3, title: 'Rust', body: 'Another programming language, i don\'t even know about.'},
+    ])
 
     return (
         <div className="App">
-            <PostItem post={{id: 1, title: 'Javascript', body: 'Programming language.'}}/>
-            <PostItem post={{id: 2, title: 'Python', body: 'Another programming language.'}}/>
+            {
+                posts.map((post) => <PostItem post={post} key={post.id}/>)
+            }
         </div>
     );
 }
