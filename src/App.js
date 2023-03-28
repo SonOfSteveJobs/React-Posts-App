a7932423c4d505e84786cfc2457178365df16322
SonOfSteveJobs <evgenev2222@gmail.com>
2023-03-28 17:25:20 +0300

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css';
 import PostList from './components/Postlist';
 import PostForm from './components/PostForm';
@@ -9,17 +9,27 @@ import {usePosts} from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/loader/Loader';
 import {useFetching} from './hooks/useFetching';
+import {getPageCount} from './utils/pages';
+import {usePagination} from './hooks/usePagination';
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostService.getAll();
-        setPosts(response);
-    })
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = (response.headers['x-total-count']);
+        setTotalPages(getPageCount(totalCount, limit));
+    });
+
+    const memoPages = usePagination(totalPages);
+
     // const bodyInputRef = useRef(); неуправляемый компоненет
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -29,7 +39,11 @@ function App() {
 
     useEffect(() => {
         fetchPosts();
-    }, []);
+    }, [page]);
+
+    const changePage = (page) => {
+        setPage(page);
+    }
 
     return (
         <div className="App">
@@ -49,6 +63,14 @@ function App() {
                 ? <div style={{marginTop: 50, justifyContent: 'center', display: 'flex'}}><Loader/></div>
                 : <PostList posts={sortedAndSearchedPosts} remove={removePost} title={'Posts list №1'}></PostList>
             }
+            <div className="page__wrapper">
+                {memoPages.map((p) => <span
+                    onClick={() => changePage(p)}
+                    key={p}
+                    className={page === p ? 'active__page' : 'page'}>
+                    {p}
+                </span>)}
+            </div>
         </div>
     );
 }
